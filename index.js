7ed3696484d87116cff2b09253399c12eb2934af
souki20 <souki0201@icloud.com>
2021-11-06 17:31:21 +0900

    枠組みと変数を追加

---
+++ index.js
@@ -0,0 +1,48 @@
+{
+  // 訪問者の年齢、性別、収入、職業、勇退年齢
+  // 性別は、0:男性、1:女性、3:その他
+  // 職業は、0:自営業、1:会社員、2:専業主婦
+  let userAge = 50;
+  let userSex = 0;
+  let userIncome = 700;
+  let userBusiness = 1;
+  let userRetireAge = 70;
+
+  // spouseは配偶者、0:あり、0:なし
+  let spouse = 1;
+  let spouseAge = 65;
+  // let spouseSex = 0;
+  let spouseIncome = 600;
+  let spouseBusiness = 1;
+  let spouseRetireAge = 70;
+
+  let childeNumber = 2;
+  let childeAgeList = [];
+  childeAgeList[0] = 21;
+  childeAgeList[1] = 17;
+
+  // houseは、0:賃貸、1:持ち家
+  // 家賃(住居費返済額)、ローン返済年数
+  let house = 0;
+  let houseCost = 25;
+  // let houseRepayment = 20;
+  let houseLoanYears = 30;
+
+  // 通信費、生活費、保険料、水道光熱費、教育費
+  let communicationCost = 3;
+  let lifeCost = 20;
+  let insuranceCost = 3;
+  let utilityCost = 5; 
+  let educationCost = 17;
+
+  // 貯蓄可能額、退職金、現在の貯金額
+  let savingsPossible = 10;
+  let severancePay = 200;
+  let nowSavings = 100;
+
+  // 年金
+  let pension = {0:78.1700, 1:172.5132, 2:78.1700};
+
+
+
+}
